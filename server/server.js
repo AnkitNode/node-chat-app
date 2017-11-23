9093666a66a9a7bee9bf49db6df5febd8813f694
unknown <C:\The Bat! Pwd>
2017-11-23 23:13:47 +0530

    init upate

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.static(publicPath));
 //   res.send(
 // '<h1>My name is Ankit</h1>');
 // })
-app.listen(3000 ,()=>{
-  console.log(`Server started on port 3000`);
+app.listen(port ,()=>{
+  console.log(`Server started on port ${port}`);
 
 })
