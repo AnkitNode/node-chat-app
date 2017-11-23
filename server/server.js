11c455c56520d01c2fcbd77a1d1e685c1491c2ed
unknown <C:\The Bat! Pwd>
2017-11-23 22:49:06 +0530

    First Git commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname,'/../public');
+const port = process.env.PORT||3000;
+
+var app = express();
+app.use(express.static(publicPath));
+
+// app.get('/',(req,res)=>{
+//   res.send(
+// '<h1>My name is Ankit</h1>');
+// })
+app.listen(3000 ,()=>{
+  console.log(`Server started on port ${port}`);
+
+})
