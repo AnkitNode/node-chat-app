46a164dd924b994b1279c919b12df1ed2514cdbd
unknown <C:\The Bat! Pwd>
2017-11-23 23:07:21 +0530

    First Git commit

--- server/server.js
+++ server/server.js
@@ -12,6 +12,6 @@ app.use(express.static(publicPath));
 // '<h1>My name is Ankit</h1>');
 // })
 app.listen(3000 ,()=>{
-  console.log(`Server started on port ${port}`);
+  console.log(`Server started on port 3000`);
 
 })
